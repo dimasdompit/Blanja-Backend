dee41e667080cd3fa1a022c377c3f39a9e659607
Dimas Mokodompit <dimasdompit@gmail.com>
2020-09-18 11:17:33 +0700

    add Colors routes

---
+++ src/routes/colors.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const router = express.Router();
+const {
+  getAllColors,
+  getColorDetails,
+  addColors,
+  updateColors,
+  deleteColors,
+} = require("../controllers/ColorsController");
+
+router.get("/", getAllColors);
+router.get("/:id", getColorDetails);
+router.post("/", addColors);
+router.put("/:id", updateColors);
+router.delete("/:id", deleteColors);
+
+module.exports = router;
--- src/routes/index.js
+++ src/routes/index.js
@@ -3,9 +3,11 @@ const router = express.Router();
 const productRoute = require("./products");
 const conditionsRoute = require("./conditions");
 const categoriesRoute = require("./categories");
+const colorsRoute = require("./colors");
 
 router.use("/api/products", productRoute);
 router.use("/api/conditions", conditionsRoute);
 router.use("/api/categories", categoriesRoute);
+router.use("/api/colors", colorsRoute);
 
 module.exports = router;
