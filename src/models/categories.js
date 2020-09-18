b3f4a9c5d0a7ab3b8caf70db2746475717186f66
Dimas Mokodompit <dimasdompit@gmail.com>
2020-09-18 10:22:34 +0700

    add categories model

---
+++ src/models/categories.js
@@ -0,0 +1,74 @@
+const connection = require("../config/database");
+const query = require("../helpers/query/categories");
+
+module.exports = {
+  getAllCategoriesModel: () => {
+    return new Promise((resolve, reject) => {
+      const sql = query.getAllCategories;
+
+      connection.query(sql, (error, result) => {
+        if (error) reject(error);
+        resolve(result);
+      });
+    });
+  },
+
+  getCategoriesDetailsModel: (id) => {
+    return new Promise((resolve, reject) => {
+      const sql = query.getCategoriesDetails;
+
+      connection.query(sql, id, (error, result) => {
+        if (error) reject(error);
+        resolve(result);
+      });
+    });
+  },
+
+  addCategoriesModel: (data) => {
+    return new Promise((resolve, reject) => {
+      const sql = query.addCategories;
+
+      connection.query(sql, data, (error, result) => {
+        if (error) reject(error);
+
+        const newData = {
+          id: result.id,
+          ...data,
+        };
+        resolve(newData);
+      });
+    });
+  },
+
+  updateCategoriesModel: (data, id) => {
+    return new Promise((resolve, reject) => {
+      const sql = query.updateCategories;
+
+      connection.query(sql, [data, id], (error, result) => {
+        if (error) reject(error);
+
+        const newData = {
+          id,
+          ...data,
+        };
+        resolve(newData);
+      });
+    });
+  },
+
+  deleteCategoriesModel: (id) => {
+    return new Promise((resolve, reject) => {
+      const sql = query.deleteCategories;
+
+      connection.query(sql, id, (error, result) => {
+        if (error) reject(error);
+
+        const newData = {
+          id,
+          ...result,
+        };
+        resolve(newData);
+      });
+    });
+  },
+};
