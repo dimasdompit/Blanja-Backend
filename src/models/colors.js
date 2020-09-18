d5a7a77eeea9fe7ee7818b2b3b87eda98b0eb58a
Dimas Mokodompit <dimasdompit@gmail.com>
2020-09-18 11:04:01 +0700

    add Colors model

---
+++ src/models/colors.js
@@ -0,0 +1,71 @@
+const connection = require("../config/database");
+const query = require("../helpers/query/colors");
+
+module.exports = {
+  getAllColorsModel: () => {
+    return new Promise((resolve, reject) => {
+      const sql = query.getAllColors;
+      connection.query(sql, (error, result) => {
+        if (error) reject(error);
+        resolve(result);
+      });
+    });
+  },
+
+  getColorDetailsModel: (id) => {
+    return new Promise((resolve, reject) => {
+      const sql = query.getColorDetails;
+      connection.query(sql, id, (error, result) => {
+        if (error) reject(error);
+        resolve(result);
+      });
+    });
+  },
+
+  addColorsModel: (data) => {
+    return new Promise((resolve, reject) => {
+      const sql = query.addColors;
+      connection.query(sql, data, (error, result) => {
+        if (error) reject(error);
+
+        const newData = {
+          id: result.id,
+          ...data,
+        };
+
+        resolve(newData);
+      });
+    });
+  },
+
+  updateColorsModel: (data, id) => {
+    return new Promise((resolve, reject) => {
+      const sql = query.updateColors;
+      connection.query(sql, [data, id], (error, result) => {
+        if (error) reject(error);
+
+        const newData = {
+          id,
+          ...data,
+        };
+        resolve(newData);
+      });
+    });
+  },
+
+  deleteColorsModel: (id) => {
+    return new Promise((resolve, reject) => {
+      const sql = query.deleteColors;
+      connection.query(sql, id, (error, result) => {
+        if (error) reject(error);
+
+        const newData = {
+          id,
+          ...result,
+        };
+
+        resolve(newData);
+      });
+    });
+  },
+};
