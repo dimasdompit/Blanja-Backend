9631f238e2c9b8c2ba89b0bca54740518d992ba9
Dimas Mokodompit <dimasdompit@gmail.com>
2020-09-18 13:57:35 +0700

    add Sizes controller

---
+++ src/controllers/SizesController.js
@@ -0,0 +1,90 @@
+const { response } = require("../helpers/response");
+const {
+  AddSizesValidation,
+  UpdateSizesValidation,
+} = require("../helpers/validation");
+const {
+  getAllSizesModel,
+  getSizesDetailsModel,
+  addSizesModel,
+  updateSizesModel,
+  deleteSizesModel,
+} = require("../models/sizes");
+
+module.exports = {
+  getAllSizes: async (req, res) => {
+    try {
+      const result = await getAllSizesModel();
+      if (result[0]) {
+        return response(res, true, result, 200);
+      }
+      return response(res, false, `Sizes Not Found`, 404);
+    } catch (error) {
+      console.log(error);
+      return response(res, false, `Internal Server Error`, 500);
+    }
+  },
+
+  getSizesDetails: async (req, res) => {
+    const id = req.params.id;
+    try {
+      const result = await getSizesDetailsModel(id);
+      if (result[0]) {
+        return response(res, true, result, 200);
+      }
+      return response(res, false, `Sizes with ID=${id} Not Found`, 404);
+    } catch (error) {
+      console.log(error);
+      return response(res, false, "Internal Server Error", 500);
+    }
+  },
+
+  addSizes: async (req, res) => {
+    const data = req.body;
+    try {
+      const validation = AddSizesValidation(data);
+      if (validation.error === undefined) {
+        const result = await addSizesModel(data);
+        return response(res, true, result, 201);
+      }
+      let errorMsg = validation.error.details[0].message;
+      errorMsg = errorMsg.replace(/"/g, "");
+      return response(res, false, errorMsg, 400);
+    } catch (error) {
+      console.log(error);
+      return response(res, false, "Internal Server Error", 500);
+    }
+  },
+
+  updateSizes: async (req, res) => {
+    const data = req.body;
+    const id = req.params.id;
+    try {
+      const validation = UpdateSizesValidation(data);
+      if (validation.error === undefined) {
+        const result = await updateSizesModel(data, id);
+        return response(res, true, result, 201);
+      }
+      let errorMsg = validation.error.details[0].message;
+      errorMsg = errorMsg.replace(/"/g, "");
+      return response(res, false, errorMsg, 400);
+    } catch (error) {
+      console.log(error);
+      return response(res, false, "Internal Server Error", 500);
+    }
+  },
+
+  deleteSizes: async (req, res) => {
+    const id = req.params.id;
+    try {
+      const result = await deleteSizesModel(id);
+      if (result.affectedRows === 1) {
+        return response(res, true, result, 200);
+      }
+      return response(res, false, `Sizes with ID = ${id} Not Found`, 404);
+    } catch (error) {
+      console.log(error);
+      return response(res, false, "Internal Server Error", 500);
+    }
+  },
+};
