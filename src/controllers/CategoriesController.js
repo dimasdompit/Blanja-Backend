ade1feefec7964029ccd9c3b95d4f2730b5e8894
Dimas Mokodompit <dimasdompit@gmail.com>
2020-09-18 10:52:24 +0700

    finishing categories controllers and routes

--- src/controllers/CategoriesController.js
+++ src/controllers/CategoriesController.js
@@ -25,4 +25,73 @@ module.exports = {
       return response(res, false, "Internal Server Error", 500);
     }
   },
+
+  getCategoriesDetails: async (req, res) => {
+    const id = req.params.id;
+    try {
+      const result = await getCategoriesDetailsModel(id);
+
+      if (result[0]) {
+        return response(res, true, result, 200);
+      }
+      return response(res, false, `Categories with ID = ${id} Not Found`, 404);
+    } catch (error) {
+      console.log(error);
+      return response(res, false, "Internal Server Error", 500);
+    }
+  },
+
+  addCategories: async (req, res) => {
+    const data = req.body;
+    try {
+      const validation = AddCategoriesValidation(data);
+
+      if (validation.error === undefined) {
+        const result = await addCategoriesModel(data);
+        return response(res, true, result, 201);
+      }
+
+      let errorMsg = validation.error.details[0].message;
+      errorMsg = errorMsg.replace(/"/g, "");
+      return response(res, false, errorMsg, 400);
+    } catch (error) {
+      console.log(error);
+      return response(res, false, "Internal Server Error", 500);
+    }
+  },
+
+  updateCategories: async (req, res) => {
+    const data = req.body;
+    const id = req.params.id;
+    try {
+      const validation = UpdateCategoriesValidation(data);
+
+      if (validation.error === undefined) {
+        const result = await updateCategoriesModel(data, id);
+        return response(res, true, result, 200);
+      }
+
+      let errorMsg = validation.error.details[0].message;
+      errorMsg = errorMsg.replace(/"/g, "");
+      return response(res, false, errorMsg, 400);
+    } catch (error) {
+      console.log(error);
+      return response(res, false, "Internal Server Error", 500);
+    }
+  },
+
+  deleteCategories: async (req, res) => {
+    const id = req.params.id;
+    try {
+      const result = await deleteCategoriesModel(id);
+
+      if (result.affectedRows === 1) {
+        return response(res, true, result, 200);
+      }
+      return response(res, false, `Categories with ID = ${id} Not Found`, 404);
+    } catch (error) {
+      console.log(error);
+      return response(res, false, "Internal Server Error", 500);
+    }
+  },
 };
--- src/routes/categories.js
+++ src/routes/categories.js
@@ -1,7 +1,17 @@
 const express = require("express");
 const router = express.Router();
-const { getAllCategories } = require("../controllers/CategoriesController");
+const {
+  getAllCategories,
+  getCategoriesDetails,
+  addCategories,
+  updateCategories,
+  deleteCategories,
+} = require("../controllers/CategoriesController");
 
 router.get("/", getAllCategories);
+router.get("/:id", getCategoriesDetails);
+router.post("/", addCategories);
+router.put("/:id", updateCategories);
+router.delete("/:id", deleteCategories);
 
 module.exports = router;
