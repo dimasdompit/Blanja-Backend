434a84574ddd78d2639d7cc500895c78b94c8bf7
Dimas Mokodompit <dimasdompit@gmail.com>
2020-09-19 22:54:08 +0700

    add Role Check middleware

---
+++ src/middlewares/RoleCheck.js
@@ -0,0 +1,17 @@
+module.exports = {
+  checkRole: (req, res, next) => {
+    try {
+      const data = req.decoded.result[0].role;
+      const result = data === 1 ? true : false;
+      if (result) {
+        next();
+      } else {
+        return res.status(401).json({
+          message: "Access Denied!",
+        });
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  },
+};
