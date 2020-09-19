dc16c94bade2075fb09b0813adc82ee59e504bd8
Dimas Mokodompit <dimasdompit@gmail.com>
2020-09-19 15:08:56 +0700

    install mustache, add send email helper

---
+++ src/helpers/sendEmail.js
@@ -0,0 +1,32 @@
+require("dotenv").config();
+const nodemailer = require("nodemailer");
+const mustache = require("mustache");
+const fs = require("fs");
+
+module.exports = {
+  sendEmail: (type, data) => {
+    const templateEmail = fs.readFileSync("./src/helpers/template.html", {
+      encoding: "utf-8",
+    });
+
+    const transporter = nodemailer.createTransport({
+      service: "gmail",
+      auth: {
+        user: process.env.USER_EMAIL,
+        pass: process.env.PASS_EMAIL,
+      },
+    });
+
+    const mailOptions = {
+      from: "dimasdoms@gmail.com",
+      to: data.email,
+      subject: type,
+      html: mustache.render(templateEmail, data),
+    };
+
+    transporter.sendMail(mailOptions, (error, info) => {
+      if (error) console.log(error);
+      console.log(`OTP Code has been sent to ${info.accepted[0]}`);
+    });
+  },
+};
