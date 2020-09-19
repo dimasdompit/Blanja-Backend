9cba71080fe34bd7b8dc5154b9fcacec6ec9f5e2
Dimas Mokodompit <dimasdompit@gmail.com>
2020-09-19 22:22:00 +0700

    add create token helper, add token check middleware

---
+++ src/middlewares/TokenCheck.js
@@ -0,0 +1,21 @@
+const jwt = require("jsonwebtoken");
+
+module.exports = (req, res, next) => {
+  let token = null;
+  try {
+    token = req.headers.authorization;
+    const decode = jwt.verify(token, process.env.JWT_KEY);
+    req.decoded = decode;
+    next();
+  } catch (error) {
+    console.log(error);
+    if (error.name == "TokenExpiredError") {
+      return res.status(401).json({
+        message: "Token Expired",
+      });
+    }
+    return res.status(401).json({
+      message: "Invalid Token",
+    });
+  }
+};
