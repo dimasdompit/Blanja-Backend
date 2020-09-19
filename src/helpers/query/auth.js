ecbc4dd39d398e050c9febb9c1e194d996120639
Dimas Mokodompit <dimasdompit@gmail.com>
2020-09-19 16:32:04 +0700

    add query OTP helper

--- src/helpers/query/auth.js
+++ src/helpers/query/auth.js
@@ -2,4 +2,8 @@ module.exports = {
   login: `SELECT * FROM users WHERE email = ?`,
   register: `INSERT INTO users SET ?`,
   updateUser: `UPDATE users SET ? WHERE email = ?`,
+  sendOTP: `INSERT INTO otp SET ?`,
+  checkOTP: "SELECT * FROM otp WHERE email = ?",
+  deleteOTP: "DELETE FROM otp WHERE email = ?",
+  updateUser: "UPDATE users SET ? WHERE email=?",
 };
