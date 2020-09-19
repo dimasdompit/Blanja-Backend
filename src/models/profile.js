30ca6c52e5f94cba99b2579fd4882671bef679be
Dimas Mokodompit <dimasdompit@gmail.com>
2020-09-19 20:28:45 +0700

    add profile models

---
+++ src/models/profile.js
@@ -0,0 +1,87 @@
+const connection = require("../config/database");
+const query = require("../helpers/query/profile");
+
+module.exports = {
+  editUserModel: (data, id) => {
+    return new Promise((resolve, reject) => {
+      const sql = query.editUser;
+
+      connection.query(sql, [data, id], (error, result) => {
+        if (error) reject(error);
+        resolve(result);
+      });
+    });
+  },
+
+  getUserModel: (id) => {
+    return new Promise((resolve, reject) => {
+      const sql = query.getData;
+
+      connection(sql, id, (error, result) => {
+        if (error) reject(error);
+        resolve(result);
+      });
+    });
+  },
+
+  addAddressModel: (data) => {
+    return new Promise((resolve, reject) => {
+      const sql = query.addAddress;
+
+      connection.query(sql, data, (error, result) => {
+        if (error) reject(error);
+
+        const newData = { id: result.insertId, ...data };
+        resolve(newData);
+      });
+    });
+  },
+
+  editAddressModel: (data, id) => {
+    return new Promise((resolve, reject) => {
+      const sql = query.editAddress;
+
+      connection.query(sql, [data, id], (error, result) => {
+        if (error) reject(error);
+
+        const newData = { id: id, ...data };
+        resolve(newData);
+      });
+    });
+  },
+
+  getMyAddressModel: (userId) => {
+    return new Promise((resolve, reject) => {
+      const sql = query.getMyAddress;
+
+      connection.query(sql, id, (error, result) => {
+        if (error) reject(error);
+        resolve(result);
+      });
+    });
+  },
+
+  getDetailMyAddressModel: (id) => {
+    return new Promise((resolve, reject) => {
+      const sql = query.getDetailMyAddress;
+
+      connection.query(sql, id, (error, result) => {
+        if (error) reject(error);
+        resolve(result);
+      });
+    });
+  },
+
+  deleteMyAddressModel: (id) => {
+    return new Promise((resolve, reject) => {
+      const sql = query.deleteMyAddress;
+
+      connection.query(sql, id, (error, result) => {
+        if (error) reject(error);
+
+        const newData = { id: id, ...result };
+        resolve(newData);
+      });
+    });
+  },
+};
