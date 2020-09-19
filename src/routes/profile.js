bb7a77d1a13408b28a99f7e7b07e86476b7447a8
Dimas Mokodompit <dimasdompit@gmail.com>
2020-09-20 02:10:04 +0700

    add edit my address profile route

--- src/routes/profile.js
+++ src/routes/profile.js
@@ -3,11 +3,13 @@ const router = express.Router();
 const {
   editProfile,
   addMyAddress,
+  editMyAddress,
 } = require("../controllers/ProfileController");
 const tokenCheck = require("../middlewares/TokenCheck");
 const ImageFilter = require("../middlewares/ImageFilterUsers");
 
 router.put("/", tokenCheck, ImageFilter, editProfile);
 router.post("/my-address", tokenCheck, addMyAddress);
+router.put("/my-address/:id", tokenCheck, editMyAddress);
 
 module.exports = router;
