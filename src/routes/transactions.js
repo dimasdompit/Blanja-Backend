d6aeab3837e03b8dbf339a1effcaf70dc4f63aba
Dimas Mokodompit <dimasdompit@gmail.com>
2020-09-20 11:17:34 +0700

    finish transaction controllers, fixing query transactions helper, finishing transactions routes

--- src/routes/transactions.js
+++ src/routes/transactions.js
@@ -1,6 +1,16 @@
 const express = require("express");
 const router = express.Router();
 const tokenCheck = require("../middlewares/TokenCheck");
-const {} = require("../controllers/TransactionsController");
+const {
+  insertTransactions,
+  getAllTransactions,
+  getMyTransactions,
+  getTransactionDetails,
+} = require("../controllers/TransactionsController");
+
+router.post("/", tokenCheck, insertTransactions);
+router.get("/", tokenCheck, getAllTransactions);
+router.get("/my-transactions", tokenCheck, getMyTransactions);
+router.get("/detail/:id", tokenCheck, getTransactionDetails);
 
 module.exports = router;
